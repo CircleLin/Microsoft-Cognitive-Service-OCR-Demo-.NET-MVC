6b694a9a15c69e7cef2f8805b5dcf17f23c21677
kwlin <kwlin@ceec.edu.tw>
2018-12-04 14:18:59 +0800

    bug fixed

--- textmvc/Controllers/HomeController.cs
+++ textmvc/Controllers/HomeController.cs
@@ -36,9 +36,9 @@ namespace textmvc.Controllers
                     TextHandler textHandler = new TextHandler(path);
 
                     //Send Request to Computer Vision API by TextHandler
-                    TextJson data = await textHandler.MakeORCRequest();      
-                   
-                    if(data.regions.FirstOrDefault() == null)
+                    TextJson data = await textHandler.MakeORCRequest();
+
+                    if (data == null || data.regions.Count() == 0)
                     {
                         TempData["message"] = "無法辨識出文字 \n Can not recognize text";
                         return View();
